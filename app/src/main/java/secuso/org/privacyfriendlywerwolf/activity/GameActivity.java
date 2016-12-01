b271b3a8aab9c5f127e36d70aba69aef0046f733
Daniel Goelz <danielgoelz@web.de>
2016-12-01 19:39:57 +0100

    added some code to xml's and LobbyActivity, and some testing code to GameActivity

--- app/src/main/java/secuso/org/privacyfriendlywerwolf/activity/GameActivity.java
+++ app/src/main/java/secuso/org/privacyfriendlywerwolf/activity/GameActivity.java
@@ -1,15 +1,35 @@
 package secuso.org.privacyfriendlywerwolf.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import secuso.org.privacyfriendlywerwolf.R;
 
 public class GameActivity extends AppCompatActivity {
+    ArrayList<String> players;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        Intent intent = getIntent();
+        players = intent.getStringArrayListExtra(LobbyActivity.EXTRA_MESSAGE);
+
+        // Ausgabe Test
+        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_game);
+        for(int i=0; i<players.size();i++) {
+            TextView textView = new TextView(this);
+            textView.setTextSize(40);
+            textView.setText(players.get(i));
+
+
+            layout.addView(textView); // Ausgabe Test Ende
+        }
     }
 }
--- app/src/main/java/secuso/org/privacyfriendlywerwolf/activity/LobbyActivity.java
+++ app/src/main/java/secuso/org/privacyfriendlywerwolf/activity/LobbyActivity.java
@@ -1,15 +1,31 @@
 package secuso.org.privacyfriendlywerwolf.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 import secuso.org.privacyfriendlywerwolf.R;
 
 public class LobbyActivity extends AppCompatActivity {
-
+    public final static String EXTRA_MESSAGE = "secuso.org.privacyfriendlywerwolf.MESSAGE";
+    private ArrayList<String> players = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
+    }
+
+    public void sendName(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_name);
+        String name = editText.getText().toString();
+        players.add(name);
+        //intent.putExtra(EXTRA_MESSAGE, name);
+        intent.putStringArrayListExtra(EXTRA_MESSAGE, players);
+        startActivity(intent);
     }
 }
