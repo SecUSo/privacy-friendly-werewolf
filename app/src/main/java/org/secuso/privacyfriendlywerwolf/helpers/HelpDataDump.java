4fbfb3841b556746a77c82168e832a0c6de69344
fstaubach <florian.staubach@stud.tu-darmstadt.de>
2017-02-26 15:13:30 +0100

    added Q&A in en and de, fixes #10

--- app/src/main/java/org/secuso/privacyfriendlywerwolf/helpers/HelpDataDump.java
+++ app/src/main/java/org/secuso/privacyfriendlywerwolf/helpers/HelpDataDump.java
@@ -25,22 +25,42 @@ public class HelpDataDump {
 
         List<String> general = new ArrayList<String>();
         general.add(context.getResources().getString(R.string.help_whatis_answer));
-
         expandableListDetail.put(context.getResources().getString(R.string.help_whatis), general);
 
-        List<String> features = new ArrayList<String>();
-        features.add(context.getResources().getString(R.string.help_feature_one_answer));
+        List<String> roles = new ArrayList<String>();
+        roles.add(context.getResources().getString(R.string.help_roles_answer));
+        expandableListDetail.put(context.getResources().getString(R.string.help_roles), roles);
 
-        expandableListDetail.put(context.getResources().getString(R.string.help_feature_one), features);
+        List<String> role_villager = new ArrayList<String>();
+        role_villager.add(context.getResources().getString(R.string.help_roles_villager_answer));
+        expandableListDetail.put(context.getResources().getString(R.string.help_roles_villager), role_villager);
+
+        List<String> role_werewolfe = new ArrayList<String>();
+        role_werewolfe.add(context.getResources().getString(R.string.help_roles_werewolfe_answer));
+        expandableListDetail.put(context.getResources().getString(R.string.help_roles_werewolfe), role_werewolfe);
+
+        List<String> role_witch = new ArrayList<String>();
+        role_witch.add(context.getResources().getString(R.string.help_roles_witch_answer));
+        expandableListDetail.put(context.getResources().getString(R.string.help_roles_witch), role_witch);
+
+        List<String> role_seer = new ArrayList<String>();
+        role_seer.add(context.getResources().getString(R.string.help_roles_seer_answer));
+        expandableListDetail.put(context.getResources().getString(R.string.help_roles_seer), role_seer);
+
+        List<String> game_sequence = new ArrayList<String>();
+        game_sequence.add(context.getResources().getString(R.string.help_sequence_answer));
+        expandableListDetail.put(context.getResources().getString(R.string.help_sequence), game_sequence);
+
+        List<String> game_end = new ArrayList<String>();
+        game_end.add(context.getResources().getString(R.string.help_end_answer));
+        expandableListDetail.put(context.getResources().getString(R.string.help_end), game_end);
 
         List<String> privacy = new ArrayList<String>();
         privacy.add(context.getResources().getString(R.string.help_privacy_answer));
-
         expandableListDetail.put(context.getResources().getString(R.string.help_privacy), privacy);
 
         List<String> permissions = new ArrayList<String>();
         permissions.add(context.getResources().getString(R.string.help_permission_answer));
-
         expandableListDetail.put(context.getResources().getString(R.string.help_permission), permissions);
 
         return expandableListDetail;
